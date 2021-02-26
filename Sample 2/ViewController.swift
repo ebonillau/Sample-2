4e0543b66b02acfb78b2a79de9c4c85218246ebb
enriquebonilla <nexen.aplicaciones@gmail.com>
2021-02-26 01:04:19 -0500

    Initial Commit

---
+++ Sample 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample 2
+//
+//  Created by Enrique Bonilla on 26/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
